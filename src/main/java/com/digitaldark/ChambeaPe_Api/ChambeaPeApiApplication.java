6e95d6a5fe96effc99db5b5867c9fce76c5c1355
Ray <ray_punkeki@hotmail.com>
2023-11-20 18:24:37 -0500

    feat: aplication configured

--- src/main/java/com/digitaldark/ChambeaPe_Api/ChambeaPeApiApplication.java
+++ src/main/java/com/digitaldark/ChambeaPe_Api/ChambeaPeApiApplication.java
@@ -1,7 +1,12 @@
 package com.digitaldark.ChambeaPe_Api;
 
+import com.digitaldark.ChambeaPe_Api.security.util.Utilities;
+import com.digitaldark.ChambeaPe_Api.user_security.model.enums.ERole;
+import com.digitaldark.ChambeaPe_Api.user_security.repository.IRoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChambeaPeApiApplication {
@@ -10,4 +15,11 @@ public class ChambeaPeApiApplication {
 		SpringApplication.run(ChambeaPeApiApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDatabase(IRoleRepository roleRepository) {
+		return args -> {
+			Utilities.insertRoleIfNotFound(roleRepository, ERole.ROLE_USER);
+			Utilities.insertRoleIfNotFound(roleRepository, ERole.ROLE_ADMIN);
+		};
+	}
 }

071c79bea7f5213e2190939ebf574e4f2f7f2f65
Ray <ray_punkeki@hotmail.com>
2023-11-20 20:41:37 -0500

    feat: swagger comments v2 added

--- src/main/java/com/digitaldark/ChambeaPe_Api/review/controller/ReviewController.java
+++ src/main/java/com/digitaldark/ChambeaPe_Api/review/controller/ReviewController.java
@@ -3,6 +3,11 @@ package com.digitaldark.ChambeaPe_Api.review.controller;
 import com.digitaldark.ChambeaPe_Api.review.dto.request.ReviewRequestDTO;
 import com.digitaldark.ChambeaPe_Api.review.dto.response.ReviewResponseDTO;
 import com.digitaldark.ChambeaPe_Api.review.service.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "ReviewController", description = "Controller to handle Review")
 @RestController
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*") // Puerto de Angular
@@ -19,13 +25,23 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+    @Operation(summary = "Get all reviews by worker id")
+    @ApiResponse(responseCode = "201",
+            description = "Successful operation, returning all reviews by worker id",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReviewResponseDTO.class)))
     @Transactional(readOnly = true)
     @GetMapping("/workers/{id}/reviews")
     public ResponseEntity<List<ReviewResponseDTO>> getAllReviewsByWorkerId(@PathVariable(value = "id") int id) {
         return new ResponseEntity<List<ReviewResponseDTO>>(reviewService.getAllReviewsByWorkerId(id), HttpStatus.OK);
     }
 
-    //crear una review
+    //crear una
+    @Operation(summary = "Create review by worker id")
+    @ApiResponse(responseCode = "201",
+            description = "Successful operation, returning created review",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReviewResponseDTO.class)))
     @Transactional
     @PostMapping("/workers/{id}/reviews")
     public ResponseEntity<ReviewResponseDTO> createReview(@RequestBody ReviewRequestDTO review, @PathVariable(value = "id") int id) {
